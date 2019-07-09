e38522eb70d1fd226a124b3de19c5dac1baac24f
kupolin <joanthan.lin108@gmail.com>
2019-07-09 08:43:35 -0700

    Vector Drawable backward compat to API 7. Vector Drawable are API 21+

--- app/src/main/java/me/jonlin/android/dicerollerkotlin/MainActivity.kt
+++ app/src/main/java/me/jonlin/android/dicerollerkotlin/MainActivity.kt
@@ -30,7 +30,6 @@ class MainActivity : AppCompatActivity() {
                 5 -> R.drawable.dice_5
                 else -> R.drawable.dice_6
             }
-
             it.setImageResource(diceId)
         }
 
